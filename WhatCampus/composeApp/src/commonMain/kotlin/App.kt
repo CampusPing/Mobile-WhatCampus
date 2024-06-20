8c90dbdd7ff80bad7a486611aa6488ccb905e301
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2024-06-20 14:39:22 +0900

    refactor: 프로젝트명을 WhatCampus로 변경 (#13)

--- WhatCampus/composeApp/src/commonMain/kotlin/App.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/App.kt
@@ -13,8 +13,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
-import writebox.composeapp.generated.resources.Res
-import writebox.composeapp.generated.resources.compose_multiplatform
+import whatcampus.composeapp.generated.resources.Res
+import whatcampus.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun App() {
