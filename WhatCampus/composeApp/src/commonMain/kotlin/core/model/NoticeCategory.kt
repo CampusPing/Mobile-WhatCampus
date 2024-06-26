de7e9aa2aca798fe52be352ae26215116f76d080
buna <tmdgh1592@naver.com>
2024-06-26 11:30:50 +0900

    feat: NoticeCategoryList 컴포넌트 구현

---
+++ WhatCampus/composeApp/src/commonMain/kotlin/core/model/NoticeCategory.kt
@@ -0,0 +1,6 @@
+package core.model
+
+data class NoticeCategory(
+    val id: Int,
+    val name: String,
+)
---
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/notice/components/NoticeCategoryList.kt
@@ -0,0 +1,72 @@
+package feature.notice.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import core.designsystem.theme.Graphite
+import core.designsystem.theme.WhatcamTheme
+import core.designsystem.theme.White
+import core.model.NoticeCategory
+
+@Composable
+internal fun NoticeCategoryList(
+    modifier: Modifier = Modifier,
+    noticeCategories: List<NoticeCategory>,
+    selectedCategory: NoticeCategory,
+    onClickCategory: (NoticeCategory) -> Unit,
+) {
+    val scrollState = rememberScrollState()
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .horizontalScroll(state = scrollState)
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        noticeCategories.forEach { category ->
+            NoticeCategoryTab(
+                category = category,
+                isSelected = selectedCategory == category,
+                onClick = { onClickCategory(category) }
+            )
+        }
+    }
+}
+
+@Composable
+private fun NoticeCategoryTab(
+    category: NoticeCategory,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = Modifier
+            .padding(horizontal = 4.dp)
+            .shadow(8.dp, RoundedCornerShape(16.dp))
+            .background(White)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 8.dp),
+        onClick = onClick,
+    ) {
+        Text(
+            text = category.name,
+            style = WhatcamTheme.typography.labelLargeR,
+            color = if (isSelected) WhatcamTheme.colors.primary else Graphite,
+            modifier = Modifier.background(color = Color.Transparent)
+        )
+    }
+}
+
