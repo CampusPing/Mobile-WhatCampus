55deaa6187d059e4b0a5fc3fd68d551603e326e4
buna <tmdgh1592@naver.com>
2024-06-27 00:41:58 +0900

    feat: 웹뷰에서 Header, Footer 태그 지우는 기능 구현

--- WhatCampus/composeApp/src/androidMain/kotlin/feature/notice/components/WebView.android.kt
+++ WhatCampus/composeApp/src/androidMain/kotlin/feature/notice/components/WebView.android.kt
@@ -1,16 +1,15 @@
 package feature.notice.components
 
 import android.annotation.SuppressLint
-import android.webkit.WebChromeClient
+import android.content.Context
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 
-
-@SuppressLint("SetJavaScriptEnabled")
 @Composable
 actual fun WebView(
     modifier: Modifier,
@@ -19,18 +18,49 @@ actual fun WebView(
     val context = LocalContext.current
 
     AndroidView(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(modifier),
         factory = {
-            WebView(context).apply {
-                webViewClient = WebViewClient()
-            }
+            createWebView(context)
         },
-        modifier = modifier,
         update = { webView ->
-            webView.webViewClient = WebViewClient()
-            webView.webChromeClient = WebChromeClient()
-            webView.setNetworkAvailable(true)
-            webView.settings.javaScriptEnabled = true
             webView.loadUrl(url)
         }
     )
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+private fun createWebView(
+    context: Context,
+): WebView = WebView(context).apply {
+    with(settings) {
+        builtInZoomControls = false
+        domStorageEnabled = true
+        javaScriptEnabled = true
+        loadWithOverviewMode = true
+        blockNetworkLoads = false
+        setSupportZoom(false)
+    }
+    webViewClient = object : WebViewClient() {
+        override fun onPageFinished(webView: WebView, url: String) {
+            super.onPageFinished(webView, url)
+            webView.removeHeaderAndFooter()
+        }
+    }
+    WebView.setWebContentsDebuggingEnabled(true)
+}
+
+private fun WebView.removeHeaderAndFooter() {
+    postDelayed({
+        loadUrl(
+            "javascript:(function() { " +
+                    "console.log('Removing headers and footers');" +
+                    "var headers = document.querySelectorAll('header');" +
+                    "headers.forEach(function(header) { if (header && header.parentNode) { header.parentNode.removeChild(header); } });" +
+                    "var footers = document.querySelectorAll('footer');" +
+                    "footers.forEach(function(footer) { if (footer && footer.parentNode) { footer.parentNode.removeChild(footer); } });" +
+                    "})()"
+        )
+    }, 10)
 }
