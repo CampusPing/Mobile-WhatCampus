523520659594a59cc9c35d59262e04a1373ac158
buna <tmdgh1592@naver.com>
2024-06-26 12:41:15 +0900

    feat: 카테고리 탭 디자인 변경

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/notice/components/NoticeCategoryList.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/notice/components/NoticeCategoryList.kt
@@ -1,19 +1,20 @@
 package feature.notice.components
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import core.designsystem.theme.Graphite
 import core.designsystem.theme.WhatcamTheme
@@ -37,6 +38,7 @@ internal fun NoticeCategoryList(
     ) {
         noticeCategories.forEach { category ->
             NoticeCategoryTab(
+                modifier = Modifier.padding(horizontal = 4.dp),
                 category = category,
                 isSelected = selectedCategory == category,
                 onClick = { onClickCategory(category) }
@@ -47,25 +49,26 @@ internal fun NoticeCategoryList(
 
 @Composable
 private fun NoticeCategoryTab(
+    modifier: Modifier = Modifier,
     category: NoticeCategory,
     isSelected: Boolean,
     onClick: () -> Unit,
 ) {
-    Button(
-        modifier = Modifier
-            .padding(horizontal = 4.dp)
-            .shadow(8.dp, RoundedCornerShape(16.dp))
-            .background(White)
-            .padding(horizontal = 16.dp, vertical = 8.dp),
-        shape = RoundedCornerShape(16.dp),
-        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 8.dp),
-        onClick = onClick,
+    val cardColor by animateColorAsState(if (isSelected) WhatcamTheme.colors.primary else White)
+    val textColor by animateColorAsState(if (isSelected) White else Graphite)
+
+    Card(
+        modifier = modifier
+            .shadow(elevation = 8.dp, shape = RoundedCornerShape(16.dp))
+            .background(color = cardColor, shape = RoundedCornerShape(16.dp))
+            .clickable { onClick() }
+            .padding(horizontal = 20.dp, vertical = 8.dp),
     ) {
         Text(
             text = category.name,
             style = WhatcamTheme.typography.labelLargeR,
-            color = if (isSelected) WhatcamTheme.colors.primary else Graphite,
-            modifier = Modifier.background(color = Color.Transparent)
+            color = textColor,
+            modifier = Modifier.background(color = cardColor)
         )
     }
 }
