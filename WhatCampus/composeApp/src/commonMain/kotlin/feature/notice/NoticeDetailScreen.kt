bbc08259cb6e2141416d3019133e89740ffc8ad6
buna <tmdgh1592@naver.com>
2024-06-28 00:23:56 +0900

    feat: 공지 상세화면에서 공지 공유하기 기능 구현

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/notice/NoticeDetailScreen.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/notice/NoticeDetailScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
 import core.common.extensions.collectAsStateMultiplatform
+import core.common.util.UrlSharer
 import core.designsystem.icons.filled.Bookmark
 import core.designsystem.icons.outlined.Bookmark
 import core.designsystem.theme.Graphite
@@ -21,14 +23,19 @@ import core.designsystem.theme.WhatcamTheme
 import core.di.koinViewModel
 import core.model.Notice
 import feature.notice.components.WebView
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.koinInject
 import whatcampus.composeapp.generated.resources.Res
 import whatcampus.composeapp.generated.resources.bookmark_tab_title
+import whatcampus.composeapp.generated.resources.ic_share
+import whatcampus.composeapp.generated.resources.share_notice
 import whatcampus.composeapp.generated.resources.university_title
 
 @Composable
 internal fun NoticeDetailScreen(
     viewModel: NoticeDetailViewModel = koinViewModel<NoticeDetailViewModel>(),
+    urlSharer: UrlSharer = koinInject<UrlSharer>(),
     modifier: Modifier = Modifier,
     notice: Notice,
     onClickBack: () -> Unit,
@@ -42,6 +49,7 @@ internal fun NoticeDetailScreen(
         onClickBack = onClickBack,
         onClickBookmark = { viewModel.toggleBookmark(notice) },
         isBookmarked = uiState.isBookmarked,
+        onClickShare = { urlSharer.shareUrl(url = notice.url) },
     )
 }
 
@@ -52,6 +60,7 @@ private fun NoticeDetailScreen(
     onClickBack: () -> Unit,
     onClickBookmark: () -> Unit,
     isBookmarked: Boolean,
+    onClickShare: () -> Unit,
 ) {
     Scaffold(
         modifier = modifier,
@@ -60,6 +69,7 @@ private fun NoticeDetailScreen(
                 onClickBack = onClickBack,
                 onClickBookmark = onClickBookmark,
                 isBookmarked = isBookmarked,
+                onClickShare = onClickShare,
             )
         },
     ) { paddingValues ->
@@ -78,6 +88,7 @@ private fun NoticeDetailTopBar(
     onClickBack: () -> Unit,
     onClickBookmark: () -> Unit,
     isBookmarked: Boolean,
+    onClickShare: () -> Unit,
 ) {
     TopAppBar(
         modifier = modifier,
@@ -98,6 +109,14 @@ private fun NoticeDetailTopBar(
                     contentDescription = stringResource(Res.string.bookmark_tab_title),
                     tint = if (isBookmarked) WhatcamTheme.colors.primary else Graphite,
                     modifier = Modifier.padding(4.dp),
+                )
+            }
+
+            IconButton(onClick = onClickShare) {
+                Icon(
+                    painter = painterResource(Res.drawable.ic_share),
+                    contentDescription = stringResource(Res.string.share_notice),
+                    tint = Graphite,
                 )
             }
         },
