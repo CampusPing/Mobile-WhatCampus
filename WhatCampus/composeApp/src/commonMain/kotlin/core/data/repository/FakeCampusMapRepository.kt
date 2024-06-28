1b809ac63fab20d02675a8e1b3efefb5b1e205ed
buna <tmdgh1592@naver.com>
2024-06-28 23:01:45 +0900

    feat: 가짜 캠퍼스맵 레퍼지토리에서 반환하는 캠퍼스맵 url 변경

--- WhatCampus/composeApp/src/commonMain/kotlin/core/data/repository/FakeCampusMapRepository.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/core/data/repository/FakeCampusMapRepository.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.flow
 class FakeCampusMapRepository : CampusMapRepository {
     override fun flowCampusMapUrl(campusId: Long): Flow<String> {
         return flow {
-            emit("https://github.com/CampusPing/Mobile-WhatCampus/assets/56534241/3e6eff8f-08a6-4785-bcfe-1a195d3e4817")
+            emit("https://postfiles.pstatic.net/MjAyNDA2MjhfMTcw/MDAxNzE5NTg1MDg2NDY3.UegfISKMypHzfHFqGw-bvWO4fUEsLyJEtQAv99cOZNIg.kVbvglyLbdN7b8B9Kr3PQzL9h05M65Qc-fMhZ_-UHo0g.PNG/%EC%83%81%EB%AA%85%EB%8C%80%ED%95%99%EA%B5%90_%EC%84%9C%EC%9A%B8_%EC%A7%80%EB%8F%84.png?type=w773")
         }
     }
 }
