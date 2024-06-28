6b559282fa339254e50328d2c7874cfb0a14c293
buna <tmdgh1592@naver.com>
2024-06-29 00:44:25 +0900

    fix: iOS 실기기에서 Room 로직 크래시 발생하는 버그 수정

--- WhatCampus/composeApp/src/iosMain/kotlin/core/database/getWhatcamDatabase.ios.kt
+++ WhatCampus/composeApp/src/iosMain/kotlin/core/database/getWhatcamDatabase.ios.kt
@@ -2,12 +2,19 @@ package core.database
 
 import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
-import platform.Foundation.NSHomeDirectory
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 fun getWhatcamDatabase(): WhatcamDatabase {
-    val dbFile = "${NSHomeDirectory()}/${WhatcamDatabase.DATABASE_NAME}"
+    val docDirectoryUrl = NSFileManager
+        .defaultManager()
+        .URLForDirectory(NSDocumentDirectory, NSUserDomainMask, null, true, null)
+    val dbFile = "${docDirectoryUrl?.path}/${WhatcamDatabase.DATABASE_NAME}"
 
     return Room.databaseBuilder<WhatcamDatabase>(
         name = dbFile,
