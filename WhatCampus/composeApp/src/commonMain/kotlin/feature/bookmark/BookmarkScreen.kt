e46f2756b094b7f3194f6b1bd17770042c3824a8
buna <tmdgh1592@naver.com>
2024-06-28 09:53:42 +0900

    feat: 삭제 할 북마크 추가/제거 기능 구현

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/bookmark/BookmarkScreen.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/bookmark/BookmarkScreen.kt
@@ -1,10 +1,22 @@
 package feature.bookmark
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.RadioButton
@@ -15,17 +27,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import core.common.extensions.collectAsStateMultiplatform
+import core.common.util.defaultDateFormatter
+import core.common.util.format
 import core.designsystem.theme.Graphite
+import core.designsystem.theme.Gray
+import core.designsystem.theme.PaleGray
 import core.designsystem.theme.WhatcamTheme
 import core.di.koinViewModel
 import core.model.Notice
+import feature.bookmark.model.BookmarkUiState
 import org.jetbrains.compose.resources.stringResource
 import whatcampus.composeapp.generated.resources.Res
 import whatcampus.composeapp.generated.resources.bookmark_cancel
 import whatcampus.composeapp.generated.resources.bookmark_edit
 import whatcampus.composeapp.generated.resources.bookmark_title
+
+private val horizontalPadding = 12.dp
 
 @Composable
 internal fun BookmarkScreen(
@@ -35,20 +57,26 @@ internal fun BookmarkScreen(
 ) {
     val uiState by viewModel.uiState.collectAsStateMultiplatform()
     var isEditMode by rememberSaveable { mutableStateOf(false) }
-    var isAllSelected by rememberSaveable { mutableStateOf(false) }
 
     BookmarkScreen(
         modifier = modifier,
         onClickEdit = { isEditMode = true },
         onClickCancel = {
             isEditMode = false
-            isAllSelected = false
+            viewModel.clearMarkedNoticesForDelete()
         },
-        onClickSelectAll = { isAllSelected = !isAllSelected },
+        onClickSelectAll = {
+            if (uiState.isAllNoticesMarkedForDelete) {
+                viewModel.clearMarkedNoticesForDelete()
+            } else {
+                viewModel.markAllNoticesForDelete()
+            }
+        },
         isEditMode = isEditMode,
-        isAllSelected = isAllSelected,
+        isAllSelected = uiState.isAllNoticesMarkedForDelete,
         onClickNotice = onNoticeClick,
-        bookmarkedNotices = uiState.notices,
+        onClickNoticeForDelete = viewModel::markBookmarkForDelete,
+        uiState = uiState,
     )
 }
 
@@ -61,7 +89,8 @@ private fun BookmarkScreen(
     isEditMode: Boolean,
     isAllSelected: Boolean,
     onClickNotice: (Notice) -> Unit,
-    bookmarkedNotices: List<Notice>,
+    onClickNoticeForDelete: (Notice) -> Unit,
+    uiState: BookmarkUiState,
 ) {
     Scaffold(
         modifier = modifier,
@@ -74,7 +103,28 @@ private fun BookmarkScreen(
                 isAllSelected = isAllSelected,
             )
         },
-    ) {
+    ) { paddingValues ->
+        LazyColumn(
+            contentPadding = paddingValues,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            itemsIndexed(
+                items = uiState.notices,
+                key = { _, notice -> notice.id },
+            ) { index, notice ->
+                BookmarkItem(
+                    notice = notice,
+                    onClick = { onClickNotice(notice) },
+                    onClickDelete = onClickNoticeForDelete,
+                    isSelectedForDelete = notice in uiState.markedNoticesForDelete,
+                    isEditMode = isEditMode,
+                )
+
+                if (index < uiState.notices.size - 1) {
+                    NoticeDivider()
+                }
+            }
+        }
 
     }
 }
@@ -125,5 +175,64 @@ private fun BookmarkTopBar(
                 }
             }
         },
+    )
+}
+
+@Composable
+private fun BookmarkItem(
+    modifier: Modifier = Modifier,
+    notice: Notice,
+    onClick: () -> Unit,
+    onClickDelete: (Notice) -> Unit,
+    isSelectedForDelete: Boolean,
+    isEditMode: Boolean,
+) {
+    Row {
+        Column(
+            modifier = modifier
+                .clickable(
+                    onClick = onClick,
+                    enabled = !isEditMode,
+                )
+                .weight(1f)
+                .padding(horizontal = horizontalPadding, vertical = 16.dp)
+                .heightIn(min = 48.dp, max = 80.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = notice.title,
+                style = WhatcamTheme.typography.bodyLargeR,
+                color = Graphite,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+            )
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            Text(
+                text = notice.datetime.format(formatter = defaultDateFormatter),
+                style = WhatcamTheme.typography.bodyMediumR,
+                color = Gray,
+            )
+        }
+
+        if (isEditMode) {
+            RadioButton(
+                selected = isSelectedForDelete,
+                onClick = { onClickDelete(notice) },
+                modifier = Modifier.align(alignment = Alignment.CenterVertically),
+            )
+        }
+    }
+}
+
+@Composable
+private fun NoticeDivider(
+    modifier: Modifier = Modifier,
+) {
+    HorizontalDivider(
+        modifier = modifier.padding(horizontal = horizontalPadding),
+        thickness = 2.dp,
+        color = PaleGray,
     )
 }
--- WhatCampus/composeApp/src/commonMain/kotlin/feature/bookmark/BookmarkViewModel.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/bookmark/BookmarkViewModel.kt
@@ -3,20 +3,60 @@ package feature.bookmark
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import core.domain.usecase.GetAllBookmarkedNoticesUseCase
+import core.model.Notice
 import feature.bookmark.model.BookmarkUiState
+import kotlinx.collections.immutable.persistentSetOf
+import kotlinx.collections.immutable.toPersistentSet
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 
 class BookmarkViewModel(
     getAllBookmarkedNotices: GetAllBookmarkedNoticesUseCase,
 ) : ViewModel() {
-    val uiState = getAllBookmarkedNotices()
-        .map { bookmarkedNotices -> BookmarkUiState(notices = bookmarkedNotices) }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5000),
-            initialValue = BookmarkUiState()
-        )
-    
+
+    private val _uiState = MutableStateFlow(BookmarkUiState())
+    val uiState: StateFlow<BookmarkUiState> = _uiState.asStateFlow()
+
+    init {
+        getAllBookmarkedNotices()
+            .map { bookmarkedNotices ->
+                _uiState.value = _uiState.value.copy(notices = bookmarkedNotices)
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = BookmarkUiState()
+            )
+            .launchIn(viewModelScope)
+    }
+
+    fun markBookmarkForDelete(bookmarkedNotice: Notice) {
+        if (bookmarkedNotice in uiState.value.markedNoticesForDelete) {
+            _uiState.update { uiState ->
+                uiState.copy(markedNoticesForDelete = uiState.markedNoticesForDelete - bookmarkedNotice)
+            }
+        } else {
+            _uiState.update { uiState ->
+                uiState.copy(markedNoticesForDelete = uiState.markedNoticesForDelete + bookmarkedNotice)
+            }
+        }
+    }
+
+    fun clearMarkedNoticesForDelete() {
+        _uiState.update { uiState ->
+            uiState.copy(markedNoticesForDelete = persistentSetOf())
+        }
+    }
+
+    fun markAllNoticesForDelete() {
+        _uiState.update { uiState ->
+            uiState.copy(markedNoticesForDelete = uiState.notices.toPersistentSet())
+        }
+    }
 }
