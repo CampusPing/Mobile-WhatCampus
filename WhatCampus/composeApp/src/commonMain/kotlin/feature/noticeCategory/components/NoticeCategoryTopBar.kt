70f7833068cdf331bd852353c859c97384ec89e1
buna <tmdgh1592@naver.com>
2024-06-30 19:53:34 +0900

    feat: 공지 카테고리 TopBar 구현

---
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/noticeCategory/components/NoticeCategoryTopBar.kt
@@ -0,0 +1,56 @@
+package feature.noticeCategory.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import core.designsystem.theme.Graphite
+import core.designsystem.theme.WhatcamTheme
+import org.jetbrains.compose.resources.stringResource
+import whatcampus.composeapp.generated.resources.Res
+import whatcampus.composeapp.generated.resources.notice_category_save
+import whatcampus.composeapp.generated.resources.notice_category_title
+import whatcampus.composeapp.generated.resources.setting_title
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun NoticeCategoryTopBar(
+    modifier: Modifier = Modifier,
+    onClickBack: () -> Unit,
+    onClickSave: () -> Unit,
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Text(
+                text = stringResource(Res.string.notice_category_title),
+                style = WhatcamTheme.typography.titleMediumB,
+                color = Graphite,
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onClickBack) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = stringResource(Res.string.setting_title),
+                    tint = Graphite,
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onClickSave) {
+                Icon(
+                    imageVector = Icons.Default.Done,
+                    contentDescription = stringResource(Res.string.notice_category_save),
+                    tint = WhatcamTheme.colors.primary,
+                )
+            }
+        }
+    )
+}
