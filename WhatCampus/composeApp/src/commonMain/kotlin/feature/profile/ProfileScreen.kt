aaa113233fa7216a70c6c3311026b1abfe36ab21
buna <tmdgh1592@naver.com>
2024-07-07 21:13:46 +0900

    feat: 프로필 화면 TopBar 구현

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/profile/ProfileScreen.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/profile/ProfileScreen.kt
@@ -1,10 +1,12 @@
 package feature.profile
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import core.common.extensions.collectAsStateMultiplatform
 import core.di.koinViewModel
+import feature.profile.components.ProfileTopBar
 
 @Composable
 fun ProfileScreen(
@@ -14,4 +16,12 @@ fun ProfileScreen(
 ) {
     val uiState by viewModel.uiState.collectAsStateMultiplatform()
 
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            ProfileTopBar(onClickBack = onClickBack)
+        },
+    ) { paddingValues ->
+
+    }
 }
