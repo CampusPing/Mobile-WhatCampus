ea6dd3b09504e3959e94ad368af0263ed2a6b311
buna <tmdgh1592@naver.com>
2024-08-04 22:55:12 +0900

    feat: 최소 1.5초 스플래시를 보여주고 온보딩 여부에 따라 `온보딩` 또는 `메인` 화면으로 이동하는 기능 구현

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/splash/SplashViewModel.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/splash/SplashViewModel.kt
@@ -1,17 +1,47 @@
 package feature.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import core.domain.repository.UserRepository
 import feature.splash.model.SplashEvent
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 class SplashViewModel(
+    userRepository: UserRepository,
 ) : ViewModel() {
     private val _uiEvent = MutableSharedFlow<SplashEvent>()
     val uiEvent: SharedFlow<SplashEvent> = _uiEvent.asSharedFlow()
 
     init {
+        viewModelScope.launch {
+            val startTime: Instant = Clock.System.now()
+            userRepository.flowUser()
+                .onEach { user ->
+                    val workedTime = Clock.System.now() - startTime
+                    val remainingTime = MIN_SPLASH_DURATION - workedTime
 
+                    if (remainingTime.isPositive()) {
+                        delay(remainingTime.inWholeMilliseconds)
+                    }
+                    _uiEvent.emit(SplashEvent.SplashDone(shouldOnboarding = (user == null)))
+                }
+                .launchIn(viewModelScope)
+        }
+    }
+
+    companion object {
+        private const val MIN_SPLASH_DURATION_MS: Long = 1500L
+        private val MIN_SPLASH_DURATION: Duration = MIN_SPLASH_DURATION_MS.toDuration(DurationUnit.MILLISECONDS)
     }
 }
