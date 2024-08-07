b830818c75007a2cb44963ea3d5bd92adf2275ed
buna <tmdgh1592@naver.com>
2024-08-07 22:07:31 +0900

    feat: 프로필 화면에서 유저 정보를 DataStore에서 불러오고 변경하도록 구현

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/profile/ProfileViewModel.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/profile/ProfileViewModel.kt
@@ -4,38 +4,28 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import core.domain.repository.UserRepository
 import feature.profile.model.ProfileUiState
-import feature.profile.model.User
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(
     private val userRepository: UserRepository,
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow(
-        ProfileUiState(
-            user = User(
-                userId = 1,
-                universityId = 1,
-                universityName = "상명대학교",
-                departmentId = 1,
-                departmentName = "컴퓨터과학과",
-                fcmToken = "temp-12345",
-                isPushNotificationAllowed = true,
-            )
+    val uiState = userRepository.flowUser()
+        .filterNotNull()
+        .map { user -> ProfileUiState(user) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ProfileUiState.EMPTY_STATE,
         )
-    )
-    val uiState = _uiState.asStateFlow()
 
-    // TODO: 이후에 Datastore에서 푸시 알림을 토글하는 기능을 추가할 예정입니다.
     fun changePushNotificationAllowed(isAllowed: Boolean) {
-        _uiState.update { state ->
-            state.copy(
-                user = state.user.copy(
-                    isPushNotificationAllowed = isAllowed,
-                )
-            )
+        viewModelScope.launch {
+            val user = uiState.value.user
+            userRepository.updateUser(user.copy(isPushNotificationAllowed = isAllowed))
         }
     }
 
--- WhatCampus/composeApp/src/commonMain/kotlin/feature/profile/model/ProfileUiState.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/profile/model/ProfileUiState.kt
@@ -1,16 +1,21 @@
 package feature.profile.model
 
+import core.model.User
+
 data class ProfileUiState(
     val user: User,
-)
-
-// TODO: 이후에 유저 정보를 저장하는 기능이 추가되면 제거될 클래스입니다.
-data class User(
-    val userId: Long,
-    val universityId: Long,
-    val universityName: String,
-    val departmentId: Long,
-    val departmentName: String,
-    val fcmToken: String,
-    val isPushNotificationAllowed: Boolean,
-)
+) {
+    companion object {
+        val EMPTY_STATE = ProfileUiState(
+            user = User(
+                userId = -1L,
+                universityId = -1L,
+                universityName = "-",
+                departmentId = -1L,
+                departmentName = "-",
+                fcmToken = "invalid",
+                isPushNotificationAllowed = false,
+            )
+        )
+    }
+}
