636968d6b0e213629a4889162680f08f4633bd73
buna <tmdgh1592@naver.com>
2024-08-15 21:50:55 +0900

    feat: 푸시 알림 수신 여부가 비활성화 되어 있으면 푸시 알림을 보내지 않도록 구현

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/app/NotifierInitializer.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/app/NotifierInitializer.kt
@@ -2,13 +2,20 @@ package feature.app
 
 import com.mmk.kmpnotifier.notification.NotifierManager
 import com.mmk.kmpnotifier.notification.PayloadData
+import core.domain.repository.UserRepository
 import core.model.Notice
 import feature.app.navigation.WhatcamNavigator
 import feature.notice.navigation.NoticeDetailDeepLink
-import io.github.aakira.napier.Napier
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDateTime
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-object NotifierInitializer {
+object NotifierInitializer : KoinComponent {
+    private val userRepository: UserRepository by inject()
+
     fun onApplicationStart() {
         onApplicationStartPlatformSpecific()
         NotifierManager.addListener(object : NotifierManager.Listener {
@@ -17,12 +24,17 @@ object NotifierInitializer {
                 val pushTitle = data["pushTitle"] as String
                 val pushMessage = data["pushMessage"] as String
 
-                NotifierManager.getLocalNotifier().notify(
-                    id = pushTitle.hashCode(),
-                    title = pushTitle,
-                    body = pushMessage,
-                    payloadData = data as Map<String, String>
-                )
+                MainScope().launch {
+                    val user = userRepository.flowUser().firstOrNull() ?: return@launch
+                    if (user.isPushNotificationAllowed.not()) return@launch
+
+                    NotifierManager.getLocalNotifier().notify(
+                        id = pushTitle.hashCode(),
+                        title = pushTitle,
+                        body = pushMessage,
+                        payloadData = data as Map<String, String>
+                    )
+                }
             }
 
             override fun onNotificationClicked(data: PayloadData) {
@@ -31,7 +43,6 @@ object NotifierInitializer {
                 val noticeTitle = data["title"] as String
                 val noticeDatetime = data["datetime"] as String
                 val noticeUrl = data["url"] as String
-                Napier.d { noticeDatetime }
 
                 val notice = Notice(
                     id = noticeId.toLong(),
