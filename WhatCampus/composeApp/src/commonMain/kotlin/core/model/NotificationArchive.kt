ccf5c8e178617f080d63179b51006c16d89b0f05
shchoi <shchoi@quber.com>
2024-08-26 15:10:37 +0900

    refactor: NotificationArchive 모델 클래스를 sealed class로 추상화
    - 공지 알림 종류가 추가될 수 있는 확장 가능성 고려하여 sealed class로 리팩터링

--- WhatCampus/composeApp/src/commonMain/kotlin/core/model/NotificationArchive.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/core/model/NotificationArchive.kt
@@ -2,7 +2,8 @@ package core.model
 
 sealed class NotificationArchive {
     data class NewNotice(
+        val id: Long,
         val notice: Notice,
         val isRead: Boolean,
-    )
+    ) : NotificationArchive()
 }
