d76c6458acc106f6d35caa6df2617ed7dfd336b3
shchoi <shchoi@quber.com>
2024-08-27 10:53:06 +0900

    feat: 동일한 알림 데이터가 DB에 추가되면 REPLACE 하도록 정책 변경

--- WhatCampus/composeApp/src/commonMain/kotlin/core/database/dao/NotificationDao.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/core/database/dao/NotificationDao.kt
@@ -3,6 +3,7 @@ package core.database.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import core.database.entity.NotificationEntity
 import core.database.entity.NotificationEntity.Companion.TABLE_NAME
@@ -13,7 +14,7 @@ interface NotificationDao {
     @Query("SELECT * FROM $TABLE_NAME ORDER BY receivedDatetime DESC")
     fun getAll(): Flow<List<NotificationEntity>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(notification: NotificationEntity)
 
     @Query("UPDATE $TABLE_NAME SET isRead = 1 WHERE id = :id")
