be1c001e2284f0766e36caaeea221765ac29ef4e
shchoi <shchoi@quber.com>
2024-08-27 10:46:30 +0900

    feat: 알림 목록을 데이터베이스에서 조회하도록 구현

--- WhatCampus/composeApp/src/commonMain/kotlin/core/data/repository/DefaultNotificationRepository.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/core/data/repository/DefaultNotificationRepository.kt
@@ -4,56 +4,25 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import core.database.dao.NotificationDao
+import core.database.entity.NotificationEntity
+import core.database.mapper.toNotification
 import core.database.mapper.toNotificationEntity
 import core.datastore.key.NotificationKey
 import core.domain.repository.NotificationRepository
-import core.model.Notice
 import core.model.Notification
 import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.datetime.LocalDateTime
 
 class DefaultNotificationRepository(
     private val dataStore: DataStore<Preferences>,
     private val notificationDao: NotificationDao,
 ) : NotificationRepository {
 
-    override fun flowNotifications(): Flow<PersistentList<Notification>> = flow {
-        val fakeUnreadNotification = Notification.NewNotice(
-            notificationId = 1L,
-            content = "[장학] 2024학년도 1학기 장학금 신청 안내",
-            isRead = false,
-            receivedDatetime = LocalDateTime(2024, 1, 1, 0, 0),
-            notice = Notice(
-                1,
-                "Notice 1",
-                LocalDateTime(2021, 1, 1, 0, 0),
-                "https://example.com/1"
-            )
-        )
-        val fakeReadNotification = Notification.NewNotice(
-            notificationId = 2L,
-            content = "[장학] C프로그래밍 수강 인원 증설 안내 / 컴퓨터과학과 학생 필독",
-            isRead = true,
-            receivedDatetime = LocalDateTime(2024, 8, 26, 0, 0),
-            notice = Notice(
-                2,
-                "Notice 2",
-                LocalDateTime(2024, 1, 1, 0, 0),
-                "https://example.com/1"
-            )
-        )
-
-        emit(
-            persistentListOf(
-                fakeUnreadNotification,
-                fakeReadNotification
-            )
-        )
-    }
+    override fun flowNotifications(): Flow<PersistentList<Notification>> = notificationDao.getAll()
+        .map { notifications -> notifications.map(NotificationEntity::toNotification) }
+        .map { notifications -> notifications.toPersistentList() }
 
     override suspend fun addNotification(notification: Notification) {
         notificationDao.insert(notification.toNotificationEntity())
