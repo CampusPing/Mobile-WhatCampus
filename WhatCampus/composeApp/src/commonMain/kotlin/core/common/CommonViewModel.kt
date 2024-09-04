7e261ec5c05c657fa627f5fccfba20a48eb2c0af
buna <tmdgh1592@naver.com>
2024-09-04 21:03:44 +0900

    refactor: CommonViewModel의 _commonUiEvent 접근제한자를 private로 변경하고, event 발행 함수를 제공하는 구조로 변경

--- WhatCampus/composeApp/src/commonMain/kotlin/core/common/CommonViewModel.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/core/common/CommonViewModel.kt
@@ -5,6 +5,22 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 open class CommonViewModel : ViewModel() {
-    protected val _commonUiEvent = MutableSharedFlow<CommonUiEvent>()
+    private val _commonUiEvent = MutableSharedFlow<CommonUiEvent>()
     val commonUiEvent = _commonUiEvent.asSharedFlow()
+
+    suspend fun sendClientErrorEvent() {
+        _commonUiEvent.emit(CommonUiEvent.ClientErrorOccurred)
+    }
+
+    suspend fun sendServerErrorEvent() {
+        _commonUiEvent.emit(CommonUiEvent.ServerErrorOccurred)
+    }
+
+    suspend fun sendNetworkErrorEvent() {
+        _commonUiEvent.emit(CommonUiEvent.NetworkErrorOccurred)
+    }
+
+    suspend fun sendOtherErrorEvent() {
+        _commonUiEvent.emit(CommonUiEvent.OtherErrorOccurred)
+    }
 }
--- WhatCampus/composeApp/src/commonMain/kotlin/feature/campusmap/CampusMapViewModel.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/campusmap/CampusMapViewModel.kt
@@ -2,19 +2,18 @@ package feature.campusmap
 
 import androidx.lifecycle.viewModelScope
 import core.common.CommonViewModel
-import core.common.CommonUiEvent
 import core.domain.repository.CampusMapRepository
 import core.domain.repository.UserRepository
 import core.model.Response
 import feature.campusmap.model.CampusMapUiState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.StateFlow
 
 class CampusMapViewModel(
     userRepository: UserRepository,
@@ -28,10 +27,10 @@ class CampusMapViewModel(
         .onEach { campusMapUrlResponse ->
             when (campusMapUrlResponse) {
                 is Response.Success -> Unit
-                Response.Failure.ClientError -> _commonUiEvent.emit(CommonUiEvent.ClientErrorOccurred)
-                Response.Failure.ServerError -> _commonUiEvent.emit(CommonUiEvent.ServerErrorOccurred)
-                Response.Failure.NetworkError -> _commonUiEvent.emit(CommonUiEvent.NetworkErrorOccurred)
-                is Response.Failure.OtherError<*> -> _commonUiEvent.emit(CommonUiEvent.OtherErrorOccurred)
+                Response.Failure.ClientError -> sendClientErrorEvent()
+                Response.Failure.ServerError -> sendServerErrorEvent()
+                Response.Failure.NetworkError -> sendNetworkErrorEvent()
+                is Response.Failure.OtherError<*> -> sendOtherErrorEvent()
             }
         }
         .map { campusMapUrlResponse ->
