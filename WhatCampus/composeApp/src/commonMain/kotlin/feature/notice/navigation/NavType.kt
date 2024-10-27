f6471543a8d4451d33637c91b5655ee01d152ebe
buna <tmdgh1592@naver.com>
2024-10-28 00:01:58 +0900

    fix: 공지 상세 화면으로 이동했을 때 'java.lang.IllegalArgumentException: Navigation destination that matches request NavDeepLinkRequest' 발생하는 버그 수정
    - Uri, DateTime은 base64 인코딩 방식으로 한 번 더 처리해야 함

--- WhatCampus/composeApp/src/commonMain/kotlin/feature/notice/navigation/NavType.kt
+++ WhatCampus/composeApp/src/commonMain/kotlin/feature/notice/navigation/NavType.kt
@@ -3,6 +3,8 @@ package feature.notice.navigation
 import androidx.core.bundle.Bundle
 import androidx.navigation.NavType
 import core.model.Notice
+import io.ktor.util.decodeBase64String
+import io.ktor.util.encodeBase64
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
@@ -10,18 +12,18 @@ internal val NoticeNavType = object : NavType<Notice>(
     isNullableAllowed = false,
 ) {
     override fun get(bundle: Bundle, key: String): Notice? {
-        return Json.decodeFromString(bundle.getString(key) ?: return null)
+        return Json.decodeFromString(bundle.getString(key)?.decodeBase64String() ?: return null)
     }
 
     override fun parseValue(value: String): Notice {
-        return Json.decodeFromString(value)
+        return Json.decodeFromString(value.decodeBase64String())
     }
 
     override fun put(bundle: Bundle, key: String, value: Notice) {
-        bundle.putString(key, Json.encodeToString(value))
+        bundle.putString(key, Json.encodeToString(value).encodeBase64())
     }
 
     override fun serializeAsValue(value: Notice): String {
-        return Json.encodeToString(value)
+        return Json.encodeToString(value).encodeBase64()
     }
 }
